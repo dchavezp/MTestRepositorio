1d990c1a71f4ea9ba7a4e2ee767a656320f64ef9
Dewitt Chavez <dschp229@gmail.com>
2018-11-14 23:10:44 -0500

    añadiendo main

--- PS-Tarea9/src/episunsa/MTest.java
+++ PS-Tarea9/src/episunsa/MTest.java
@@ -1,12 +1,47 @@
 package episunsa;
+
+import java.util.ArrayList;
+
 public class MTest {
-	public static void main(String [] args) {
-		PreguntasExamen pe=new PreguntasExamen(50);
-		RespuestaEstudiante re=new RespuestaEstudiante(pe,2);
+	private PreguntasExamen pe;
+	private RespuestaEstudiante re;
+	private double mediana;
+	private double varianza;
+	ArrayList<Integer> calfifaciones;
+	
+	public MTest() {
+		this.pe=new PreguntasExamen();
+		this.re=new RespuestaEstudiante();
+		this.mediana=0.0;
+		this.varianza=0.0;
+		this.calfifaciones=new ArrayList<>();
+	}
+	public MTest(int ne,int np) {
+		this.pe=new PreguntasExamen(np);
+		this.re=new RespuestaEstudiante(pe,ne);
+		this.mediana=0.0;
+		this.varianza=0.0;
+		this.calfifaciones=new ArrayList<>();
+	}
+	public double getMediana() {
+		return mediana;
+	}
+	public void setMediana(double mediana) {
+		this.mediana = mediana;
+	}
+	public double getVarianza() {
+		return varianza;
+	}
+	public void setVarianza(double varianza) {
+		this.varianza = varianza;
+	}
+	public void inicializar() {
 		pe.setPreguntas();
 		System.out.println(pe.toString());
 		re.setEstudiantesRespuestas();
 		System.out.println(re.toString());
-		
+	}
+	private int calificacion(String rp,String er) {
+		return 0;
 	}
 }
---
+++ PS-Tarea9/src/episunsa/Main.java
@@ -0,0 +1,8 @@
+package episunsa;
+
+public class Main {
+	public static void main(String[]args) {
+		MTest mt=new MTest(2, 6);
+		mt.inicializar();
+	}
+}
